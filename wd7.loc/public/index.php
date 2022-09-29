517137e02fb419f8f68ee3fa6c849a82f1398d88
PetrovichS <piatrovichs@gmail.com>
2022-09-29 22:00:21 +0300

    add leson 29.09.2022

--- wd7.loc/public/index.php
+++ wd7.loc/public/index.php
@@ -41,6 +41,41 @@ for ($i=0;$i<count($arr);$i++){
     }
 }
 echo $sum;
+echo "<br>";
+
+
+$pu= "c:\\windows\\ololo\\folder\\ololo.jpg";
+$arr = explode('\\',$pu);
+print_r($arr);
+$x = $arr[count($arr)-1];
+echo $x;
+echo "<br>";
+
+
+$pas= "gxgfjnfg.tdghtdj?gdsshhs!gfnn.";
+$name= str_replace("[?,!]",".",$pas);
+$nnn = explode('.',$pas);
+$sss= count($nnn)-1;
+echo $sss;
+echo "<br>";
+
+$email= "dsgsd@gmail.com";
+$userName=explode('@',$email);
+echo $userName[0];
+
+//https://www.php.net/manual/ru/ref.array.php
+//count — Подсчитывает количество элементов массива или Countable объекте
+// array_pop — Извлекает последний элемент массива
+//array_keys — Возвращает все или некоторое подмножество ключей массива
+//array_values — Выбирает все значения массива
+//array_slice — Выбирает срез массива
+// asort — Сортирует массив в порядке возрастания и поддерживает ассоциацию индексов
+//sort — Сортирует массив по возрастанию
+//array_diff — Вычислить расхождение массивов
+//in_array — Проверяет, присутствует ли в массиве значение
+//array_merge — Сливает один или большее количество массивов
+//array_sum — Вычисляет сумму значений массива
+//
 
 
 
