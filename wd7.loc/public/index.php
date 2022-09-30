2808d7af7fad254174bff3b3d35bf76968c3b768
PetrovichS <piatrovichs@gmail.com>
2022-09-30 21:41:48 +0300

    add header

--- wd7.loc/public/index.php
+++ wd7.loc/public/index.php
@@ -9,80 +9,13 @@
     <title>PiatrovichS in IT</title>
     <link rel="stylesheet" href="scss/styles.css">
 </head>
-<body>
-    <?php include ('header.html')?>
-<!--    --><?php //include __DIR__.'/header.html'?>
+    <body>
+         <?php include __DIR__. '/header.html'?>
 
-<br>
-
-<?php
-//php artisan serve
-echo "Петрович Сергей Николаевич";
-
-$arr=[];
-$i = 1;
-while ($i < 10) {
-    $arr[$i]= random_int(1,100);
-    echo $arr[$i];
-    echo "<br>";
-    $i++;
-}
-$arr = [
-    [1,2,3,4,5,6,7],
-    [8,9,10,11],
-    [12,15,17,111],
-];
-echo "<br>";
-$sum=0;
-for ($i=0;$i<count($arr);$i++){
-//    var_dump($arr[$i]);
-    for ($j=0;$j<count($arr[$i]);$j++){
-      $sum+= $arr[$i][$j];
-    }
-}
-echo $sum;
-echo "<br>";
+            @yield('main_content')
 
 
-$pu= "c:\\windows\\ololo\\folder\\ololo.jpg";
-$arr = explode('\\',$pu);
-print_r($arr);
-$x = $arr[count($arr)-1];
-echo $x;
-echo "<br>";
-
-
-$pas= "gxgfjnfg.tdghtdj?gdsshhs!gfnn.";
-$name= str_replace("[?,!]",".",$pas);
-$nnn = explode('.',$pas);
-$sss= count($nnn)-1;
-echo $sss;
-echo "<br>";
-
-$email= "dsgsd@gmail.com";
-$userName=explode('@',$email);
-echo $userName[0];
-
-//https://www.php.net/manual/ru/ref.array.php
-//count — Подсчитывает количество элементов массива или Countable объекте
-// array_pop — Извлекает последний элемент массива
-//array_keys — Возвращает все или некоторое подмножество ключей массива
-//array_values — Выбирает все значения массива
-//array_slice — Выбирает срез массива
-// asort — Сортирует массив в порядке возрастания и поддерживает ассоциацию индексов
-//sort — Сортирует массив по возрастанию
-//array_diff — Вычислить расхождение массивов
-//in_array — Проверяет, присутствует ли в массиве значение
-//array_merge — Сливает один или большее количество массивов
-//array_sum — Вычисляет сумму значений массива
-//
-
-
-
-?>
-
-
-<?php include ('footer.html') ?>
-<?php include ('script.html')?>
-</body>
+        <?php include __DIR__. '/footer.html' ?>
+        <?php include __DIR__. '/script.html'?>
+    </body>
 </html>
