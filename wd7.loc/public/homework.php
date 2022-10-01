a96a95d2633344657e6ac30511236b810136e774
PetrovichS <piatrovichs@gmail.com>
2022-10-01 21:41:04 +0300

    add homework 1

--- wd7.loc/public/homework.php
+++ wd7.loc/public/homework.php
@@ -7,84 +7,61 @@
 <?php include __DIR__. '/header.html'?>
 
     <main class="main">
-        <div class="container">
+
+
             <section class="main__title">
-                <h1 class="main__title-homework">Решения д/з Петровича Сергея Николаевича</h1>
-            </section>
-            <section class="main__homework-1">
-                <h1 class="main__title-homework-1">Д/з №1</h1>
-                <div>
-                    <?php
-                    echo "Петрович Сергей Николаевич";
-
-                    $arr=[];
-                    $i = 1;
-                    while ($i < 10) {
-                        $arr[$i]= random_int(1,100);
-                        echo $arr[$i];
-                        echo "<br>";
-                        $i++;
-                    }
-                    $arr = [
-                        [1,2,3,4,5,6,7],
-                        [8,9,10,11],
-                        [12,15,17,111],
-                    ];
-                    echo "<br>";
-                    $sum=0;
-                    for ($i=0;$i<count($arr);$i++){
-//    var_dump($arr[$i]);
-                        for ($j=0;$j<count($arr[$i]);$j++){
-                            $sum+= $arr[$i][$j];
-                        }
-                    }
-                    echo $sum;
-                    echo "<br>";
-
-
-                    $pu= "c:\\windows\\ololo\\folder\\ololo.jpg";
-                    $arr = explode('\\',$pu);
-                    print_r($arr);
-                    $x = $arr[count($arr)-1];
-                    echo $x;
-                    echo "<br>";
-
-
-                    $pas= "gxgfjnfg.tdghtdj?gdsshhs!gfnn.";
-                    $name= str_replace("[?,!]",".",$pas);
-                    $nnn = explode('.',$pas);
-                    $sss= count($nnn)-1;
-                    echo $sss;
-                    echo "<br>";
-
-                    $email= "dsgsd@gmail.com";
-                    $userName=explode('@',$email);
-                    echo $userName[0];
-
-                    //https://www.php.net/manual/ru/ref.array.php
-                    //count — Подсчитывает количество элементов массива или Countable объекте
-                    // array_pop — Извлекает последний элемент массива
-                    //array_keys — Возвращает все или некоторое подмножество ключей массива
-                    //array_values — Выбирает все значения массива
-                    //array_slice — Выбирает срез массива
-                    // asort — Сортирует массив в порядке возрастания и поддерживает ассоциацию индексов
-                    //sort — Сортирует массив по возрастанию
-                    //array_diff — Вычислить расхождение массивов
-                    //in_array — Проверяет, присутствует ли в массиве значение
-                    //array_merge — Сливает один или большее количество массивов
-                    //array_sum — Вычисляет сумму значений массива
-
-                    ?>
-
+                <div class="container">
+                    <h1 class="main__title-homework">Решения д/з Петровича Сергея Николаевича</h1>
                 </div>
             </section>
-        </div>
+
+            <section class="main__homework-1">
+             <div class="container">
+                <h1 class="main__title-homework-1">Д/з №1</h1>
+                <div class="main__decision-1">
+                    <div class="main__task-1">
+                        <h3 class="main__title">Вывести на экран через echo  ФИО. И выгрузить в Gidhub.</h3>
+                    </div>
+                    <div class="main__dec-1">
+                        <h2 class="main__title">Решения д/з №1</h2>
+                        <ul class="main__decision-1__ul">
+                            <li class="main__dec-1__li">1. echo "Петрович Сергей Николаевич";</li>
+                            <li class="main__dec-1__li">2. https://github.com/PiatrovichS/wd7_petrovichs </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+        <section class="main__homework-2">
+            <div class="container">
+                <h1 class="main__title-homework-2">Д/з №2</h1>
+                <div class="main__decision-2">
+                    <div class="main__task-2">
+                        <h2 class="main__title">Задание</h2>
+                        <ul class="main__decision-2__ul">
+                            <li class="main__dec-2__li">1. изменить регистр строки на противоположный (изначальная строка должна быть в верхнем или нижнем регистре)</li>
+                            <li class="main__dec-2__li">2. посчитать количество слов в строке </li>
+                            <li class="main__dec-2__li">3. получите первое слово каждого предложения в тексте. </li>
+                            <li class="main__dec-2__li">4. переведите строку в транслит </li>
+                            <li class="main__dec-2__li">5. * вывести таблицу умножения </li>
+                        </ul>
+                    </div>
+                    <div class="main__dec-2">
+                        <h2 class="main__title">Решения д/з №2</h2>
+                        <ul class="main__decision-2__ul">
+                            <li class="main__dec-2__li">????????????</li>
+                            <li class="main__dec-2__li">????????????</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 
 
 
-
-<?php include __DIR__. '/footer.html' ?>
 <?php include __DIR__. '/script.html'?>
 </body>
 </html>
--- wd7.loc/public/training.php
+++ wd7.loc/public/training.php
@@ -12,6 +12,74 @@
             <h1 class="main__title-training">Лекции Петровича Сергея Николаевича</h1>
         </section>
 
+        <section class="main__training-1">
+
+            <div class="main__training-text">
+                <p>Здесь написа что я делал</p>
+                <?php
+                echo "Петрович Сергей Николаевич";
+
+                $arr=[];
+                $i = 1;
+                while ($i < 10) {
+                    $arr[$i]= random_int(1,100);
+                    echo $arr[$i];
+                    echo "<br>";
+                    $i++;
+                }
+                $arr = [
+                    [1,2,3,4,5,6,7],
+                    [8,9,10,11],
+                    [12,15,17,111],
+                ];
+                echo "<br>";
+                $sum=0;
+                for ($i=0;$i<count($arr);$i++){
+//    var_dump($arr[$i]);
+                    for ($j=0;$j<count($arr[$i]);$j++){
+                        $sum+= $arr[$i][$j];
+                    }
+                }
+                echo $sum;
+                echo "<br>";
+
+
+                $pu= "c:\\windows\\ololo\\folder\\ololo.jpg";
+                $arr = explode('\\',$pu);
+                print_r($arr);
+                $x = $arr[count($arr)-1];
+                echo $x;
+                echo "<br>";
+
+
+                $pas= "gxgfjnfg.tdghtdj?gdsshhs!gfnn.";
+                $name= str_replace("[?,!]",".",$pas);
+                $nnn = explode('.',$pas);
+                $sss= count($nnn)-1;
+                echo $sss;
+                echo "<br>";
+
+                $email= "dsgsd@gmail.com";
+                $userName=explode('@',$email);
+                echo $userName[0];
+
+                //https://www.php.net/manual/ru/ref.array.php
+                //count — Подсчитывает количество элементов массива или Countable объекте
+                // array_pop — Извлекает последний элемент массива
+                //array_keys — Возвращает все или некоторое подмножество ключей массива
+                //array_values — Выбирает все значения массива
+                //array_slice — Выбирает срез массива
+                // asort — Сортирует массив в порядке возрастания и поддерживает ассоциацию индексов
+                //sort — Сортирует массив по возрастанию
+                //array_diff — Вычислить расхождение массивов
+                //in_array — Проверяет, присутствует ли в массиве значение
+                //array_merge — Сливает один или большее количество массивов
+                //array_sum — Вычисляет сумму значений массива
+
+                ?>
+
+            </div>
+        </section>
     </div>
 </main>
 
